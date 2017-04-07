6d831765f05d134b5a2090a61563ad6e98cdba68
u1357447 <u1357447@unimail.hud.ac.uk>
2017-04-07 22:14:00 +0100

    create index.html page

--- src/main/java/com/assignment2/controller/HomeController.java
+++ src/main/java/com/assignment2/controller/HomeController.java
@@ -12,6 +12,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HomeController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index(){
-        return "Hello, world";
+        return "index";
     }
 }
